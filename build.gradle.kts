7910ad20475aaecb352ace1c1e581bab553b4900
yolocat-dev <developer@yolocat.dev>
2023-04-10 21:50:40 +0200

    Added Lombok cuz it's Lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,12 @@
 plugins {
     kotlin("jvm") version "1.8.0"
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+}
